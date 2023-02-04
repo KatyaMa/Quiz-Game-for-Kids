2bd76d0ba933110c995804d6b652c4f54917fe49
KatyaMa <71144178+KatyaMa@users.noreply.github.com>
2023-02-03 19:36:31 -0800

    SBA project fixed

--- script.js
+++ script.js
@@ -75,69 +75,48 @@
 
 
 
-window.onload = function() {
-    let quiz = [];
-    let currentQuestion = 0;
-    let score = 0;
+    window.onload = function() {
+      let quiz = [];
+      let currentQuestion = 0;
+      let score = 0;
     
-    const questionElement = document.getElementById("question");
-    const answersElement = document.getElementById("answers");
-    const submitButton = document.getElementById("submit-button");
-    const resultElement = document.getElementById("result");
-  
-    fetch("https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple")
-      .then(response => response.json())
-      .then(data => {
-        data.results.forEach(result => {
-          const answers = {};
-          result.incorrect_answers.forEach((answer, index) => {
-            answers[String.fromCharCode(97 + index)] = answer;
+      const questionElement = document.getElementById("question");
+      const answersElement = document.getElementById("answers");
+      const submitButton = document.getElementById("submit-button");
+      const resultElement = document.getElementById("result");
+    
+      fetch("https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple")
+        .then(response => response.json())
+        .then(data => {
+          data.results.forEach(result => {
+            const answers = {};
+            result.incorrect_answers.forEach((answer, index) => {
+              answers[String.fromCharCode(97 + index)] = answer;
+            });
+            answers["c"] = result.correct_answer;
+            quiz.push({
+              question: result.question,
+              answers: answers,
+              correctAnswer: "c"
+            });
           });
-          answers["c"] = result.correct_answer;
-          quiz.push({
-            question: result.question,
-            answers: answers,
-            correctAnswer: "c"
-          });
+          displayQuestion();
         });
-        displayQuestion();
-      });
-  
-    function displayQuestion() {
-      const currentQuiz = quiz[currentQuestion];
-      questionElement.innerHTML = currentQuiz.question;
-      answersElement.innerHTML = "";
-      for (const key in currentQuiz.answers) {
-        answersElement.innerHTML +=
-          `<label><input type="radio" name="answer" value="${key}">` +
-          currentQuiz.answers[key] +
-          "</label><br>";
+    
+      function displayQuestion() {
+        const currentQuiz = quiz[currentQuestion];
+        questionElement.innerHTML = currentQuiz.question;
+        answersElement.innerHTML = "";
+        for (const key in currentQuiz.answers) {
+          answersElement.innerHTML +=
+            `<label><input type="radio" name="answer" value="${key}">` +
+            currentQuiz.answers[key] +
+            "</label><br>";
+        }
+        submitButton.style.display = "inline-block";
       }
-      submitButton.style.display = "inline-block";
-    }
-  
-    submitButton.addEventListener("click", function() {
-      const answer = document.querySelector('input[name="answer"]:checked').value;
-      if (answer === quiz[currentQuestion].correctAnswer) {
-        score++;
-      }
-      resultElement.innerHTML = "Correct answers: " + score;
-      currentQuestion++;
-      if (currentQuestion === quiz.length) {
-        resultElement.innerHTML += "<br>Quiz Completed!";
-        submitButton.style.display = "none";
-      } else {
-        displayQuestion();
-      }
-    });
-
-
-
-    // This will add the play again button when the quiz is completed, 
-    // and when you click on the play again button, it will reset the score and currentQuestion, 
-    // clear the result, and display the first question again.
-
-    submitButton.addEventListener("click", function() {
+    
+      submitButton.addEventListener("click", function() {
         const selectedOption = document.querySelector('input[name="answer"]:checked');
         if (!selectedOption) {
           resultElement.innerHTML = "Please select an answer.";
@@ -164,11 +143,5 @@ window.onload = function() {
           }
         }
       });
-    
-    
-    
-    
-      
-      
-      
-  };
+
+    };
