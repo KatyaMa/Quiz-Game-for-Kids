2c4d967c923d094d1cacdda9762a410c0d198407
KatyaMa <71144178+KatyaMa@users.noreply.github.com>
2023-02-08 17:53:09 -0800

    about me page added

---
+++ kba.js
@@ -0,0 +1,162 @@
+// let katya =
+
+// {
+//     "Name": "Ekaterina Machneva",
+//     "Location": "Roseville, CA",
+//     "Occupation": "Full Stack Java Developer",
+//     "Contact information": {
+//         "Phone": "202-361-6920",
+//         "Email": "machneva115@gmail.com",
+//         "LinkedIn": "https://www.linkedin.com/in/ekaterina-machneva",
+//         "GitHub": "https://github.com/KatyaMa/SBA"
+//     }
+// }
+
+
+
+
+
+// var melody = [7, 8, 9, 10, 11,  ];
+// melody.forEach(function(value, index, array) {
+//     array[index] = value * value
+// });
+// console.log(melody.reverse().filter(function(x) {
+//     return x % 5
+// }));
+
+
+// var thang = new Date("Thu Jan 29 2009 17:31:44");
+// var samuel = new Date("2019-01-24 18:31:44");
+// console.log(samuel.getMonth() === thang.getMonth()); // true
+
+
+// var mario = "1+2, 2+3, 3+4, 4+5, 5 + 1".split(/\s*,\s*/).reverse(); 
+// console.log(mario[1][0]+mario[1][2]); // 45
+
+
+// var violetta = ["10+1", "-5.0+-3.0", "0000.2+0.5",
+//     "3.0+6", "d+f"];
+
+// for (var k = 0; k < violetta.length; k++) {
+//     try {
+//         console.log(eval(violetta[k]));
+//     } catch (e) {
+//         console.log("Nah, bruh.");
+//     }
+// }
+// 11 -8 Nah, bruh. 9 Nah, bruh.
+
+
+
+// var arr = [1, 2, 3, 4, 5, ];
+// arr.forEach(function(value, index, array) {
+//     array[index] = value * value
+// });
+// console.log(arr.reverse().filter(function(x) {
+//     return x % 5
+// }));  // [ 16, 9, 4, 1 ]
+
+
+// var data = "1+2, 2+3, 3+4, 4+5, 5 + 1 ".split(/\s*,\s*/).reverse(); 
+
+// console.log(data[1][0]+data[1][2]);  // 45
+
+// let name = "Per Scholas";
+// let name = "JavaScript";
+
+// console.log(name); // SyntaxError
+
+
+// var a = [9, 8, 7, 6, 5, 4];
+
+// console.log(a.splice(3));
+// console.log(a);
+
+
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+// };
+
+// for (var i in person) {
+//     console.log(i);
+// }
+
+// var i = 4
+// console.log(i!=5);
+
+
+// console.log("Hello" + " World");
+
+
+// var a = "Per Scholas";
+// var x = a.lastIndexOf("l");
+// console.log(x);
+
+
+// numbers = "12345";
+// for (var i = 0; i < 5; i++) {
+//     if (numbers[i] == 3) {
+//         break;
+//     }
+//     console.log(numbers[i]);
+// }
+
+// var date = new Date("Thu Jan 29 2009 17:31:44");
+// var now = new Date("2019-01-24 18:31:44");
+
+// console.log(now.getMonth() === date.getMonth());
+
+
+// console.log("Hello" - " World");
+
+
+
+// var arr = ["9+1", "-5.0+-2.0", "0000.2+0.5",
+//     "3.0+2", "b+c"];
+
+// for (var i = 0; i < arr.length; i++) {
+//     try {
+//         console.log(eval(arr[i]));
+//     } catch (e) {
+//         console.log("Invalid");
+//     }
+// }
+
+// a = 8 + "8";
+// console.log(a);
+
+
+// var x = 5;
+// var y = 6;
+// var res = eval("x*y");
+
+// console.log(res);
+
+
+// var a = [4, 8, 5, 7, 6, 9];
+
+// console.log(a.slice(3).sort());
+// console.log(a);
+
+
+// var name = "Per Scholas";
+// var name = "JavaScript";
+
+// console.log(name);
+
+// numbers = "12345";
+// for (var i = 0; i < 5; i++) {
+//     if (numbers[i] == 3) {
+//         continue;
+//     }
+//     console.log(numbers[i]);
+// }
+
+
+var a = "Per Scholas";
+var result = a.substring(4, 5);
+
+console.log(result);
